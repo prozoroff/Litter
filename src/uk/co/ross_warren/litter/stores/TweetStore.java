3e726978f171ed6b9b1276b831f1b0af94573fc2
Ross Warren <rosswarren4@gmail.com>
2011-02-14 00:39:11 +0000

    Update web.xml for tweet servlet, added tweet servlet, and updated tweetstore bean to have getters and setters.

--- src/uk/co/ross_warren/litter/stores/TweetStore.java
+++ src/uk/co/ross_warren/litter/stores/TweetStore.java
@@ -5,4 +5,29 @@ public class TweetStore {
 	private String user = "";
 	private String timeStamp = "";
 	private String replyToUser = "";
+	
+	public void setTweetID(int tweetID) {
+		this.tweetID = tweetID;
+	}
+	public int getTweetID() {
+		return tweetID;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getUser() {
+		return user;
+	}
+	public void setTimeStamp(String timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+	public String getTimeStamp() {
+		return timeStamp;
+	}
+	public void setReplyToUser(String replyToUser) {
+		this.replyToUser = replyToUser;
+	}
+	public String getReplyToUser() {
+		return replyToUser;
+	}
 }
