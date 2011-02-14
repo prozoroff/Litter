9e5987be051ddf901b15524f9147996241ec384d
Ross Warren <rosswarren4@gmail.com>
2011-02-14 00:35:05 +0000

    Added necessary fields.

--- src/uk/co/ross_warren/litter/stores/TweetStore.java
+++ src/uk/co/ross_warren/litter/stores/TweetStore.java
@@ -3,4 +3,6 @@ package uk.co.ross_warren.litter.stores;
 public class TweetStore {
 	private int tweetID = 0;
 	private String user = "";
+	private String timeStamp = "";
+	private String replyToUser = "";
 }
